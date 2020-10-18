7eaf7aa80eb03654af349043daf85ea236114f71
rukavi4kamax1 <rukavi4ka.max@gmail.com>
2020-10-18 09:36:18 +0300

    Some corrections

--- src/main/java/homework/homework/equals_hashcode_practice/EqualsHashCodePractice.java
+++ src/main/java/homework/homework/equals_hashcode_practice/EqualsHashCodePractice.java
@@ -1,13 +1,13 @@
 package homework.homework.equals_hashcode_practice;
 
+import java.util.List;
+import homework.homework.Filling;
 import homework.homework.Order;
 import homework.homework.Pizza;
 import homework.homework.Waiter;
 import homework.homework.enums.Cheese;
 import homework.homework.enums.Meat;
 import homework.homework.enums.Vegetables;
-
-import java.util.List;
 
 public class EqualsHashCodePractice {
 
@@ -17,9 +17,9 @@ public class EqualsHashCodePractice {
     новий унікальний хешкод*/
     public boolean nonOverriddenHashcode() {
         Pizza pizza1 = new Pizza(new Pizza.Dough("yeast"),
-                new Pizza.Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN));
+                new Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN));
         Pizza pizza2 = new Pizza(new Pizza.Dough("yeast"),
-                new Pizza.Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN));
+                new Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN));
         int num1 = pizza1.hashCode();
         int num2 = pizza2.hashCode();
         return num1 == num2;
@@ -29,8 +29,8 @@ public class EqualsHashCodePractice {
         Filling. В результаті функція повертає true,
     оскільки методи перевизначені і об'єкти і рівними*/
     public boolean overriddenHashcode() {
-        Pizza.Filling filling1 = new Pizza.Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN);
-        Pizza.Filling filling2 = new Pizza.Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN);
+        Filling filling1 = new Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN);
+        Filling filling2 = new Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN);
         int num1 = filling1.hashCode();
         int num2 = filling2.hashCode();
         return num1 == num2;
@@ -41,7 +41,7 @@ public class EqualsHashCodePractice {
     оскільки методи перевизначені і об'єкти і рівними*/
     public boolean overriddenEquals() {
         Pizza pizza = new Pizza(new Pizza.Dough("yeast"),
-                new Pizza.Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN));
+                new Filling(Vegetables.OLIVES, Cheese.MOZZARELLA, Meat.CHICKEN));
         Order order1 = new Order(List.of(pizza));
         Order order2 = new Order(List.of(pizza));
         return order1.equals(order2);
